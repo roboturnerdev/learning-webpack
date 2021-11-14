a1aa3330e2da0e1ba0ce7a0952eba38d1cfd36a8
Robert Turner <roboturnerdev@gmail.com>
2021-11-14 05:09:17 -0500

    added image and font bundling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import RobSax from './robsax.jpeg';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const myRobert = new Image();
+    myRobert.src = RobSax;
+
+    element.appendChild(myRobert);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,29 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: 'development'
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],    // style then css, or errors
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
+
+// Module loaders can be chained. Each loader in the chain 
+// applies transformations to the processed resource. A chain is 
+// executed in reverse order. The first loader passes its result 
+// (resource with applied transformations) to the next one, and so forth. 
+// Finally, webpack expects JavaScript to be returned by the last loader in the chain.
 
 // config file allows far more flexibility than cli
 // can:
